b86710e1b680b29044ca4f7554d3bef3e3a37b3e
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-12-08 22:57:48 +0530

    Z (#2)

--- app/src/main/java/com/theflexproject/thunder/Constants.java
+++ app/src/main/java/com/theflexproject/thunder/Constants.java
@@ -14,9 +14,7 @@ public class Constants {
 
     public static String getFanartApiKey(){
         final String[] KEYS = {
-                "",
-                "",
-                ""};
+                "5c2e0ee6a22231d910766a1cf166d580"};
         Random random = new Random();
         int index = random.nextInt(KEYS.length);
         return KEYS[index];
